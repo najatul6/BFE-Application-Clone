5ce6faa845143c4e96c1088a3acd23fe799e1a3a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-02 01:26:31 +0600

    update  app name and port

--- application/config/app_config.py
+++ application/config/app_config.py
@@ -4,3 +4,11 @@ from pweb import PWebAppConfig
 class Config(PWebAppConfig):
     APP_NAME = "BFE-Application-Clone"
     PORT: int = 1212
+    
+    CONNECT_MESSAGE: str = "PWeb is an Open Source Python based Web Framework for Rapid Development."
+    DEVELOPED_BY: str = "Najatul islam"
+    DEVELOPED_BY_LINK: str = "https://najatul-islam.vercel.app"
+    APP_VERSION: str = "v1.0.0"
+    ENABLE_REGISTRATION: bool = True
+
+    
