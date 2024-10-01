d01db59feaec01ebfda48d8a0c4660498606e2b5
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-02 01:21:03 +0600

    update module_registry and set some module

--- application/config/module_registry.py
+++ application/config/module_registry.py
@@ -1,8 +1,10 @@
 from pweb import PWebModuleRegister
 from boot.boot_module import BootModule
-
+from pweb_ssr.pweb_ssr_module import PWebSSRModule
+from pweb_auth import PWebAuthModule
+from pweb_ui.pweb_ui_module import PWebUIModul
 
 class Register(PWebModuleRegister):
 
     def get_module_list(self) -> list:
-        return [BootModule]
+        return [BootModule,PWebSSRModule,PWebUIModule,PWebAuthModule,]
